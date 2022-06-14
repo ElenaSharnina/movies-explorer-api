6fd7d7be04ea1c0e89649d35c80aecd6862a8e97
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-06-14 18:58:49 +0300

    созданы схемы и модели

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.listen(PORT, () => {
-  // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
-});
+async function main() {
+  mongoose.connect('mongodb://localhost:27017/moviedb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: false,
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Cлушаем ${PORT} порт`);
+  });
+}
+
+main();
